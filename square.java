f8a6f152655bcfa0c4f38163ff761c9ad33ca7a5
rohitwarade <waraderp@gmail.com>
2023-05-10 09:53:41 +0530

    initial commit

---
+++ square.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+ 
+public class square {
+    public static void main(String args[]) {
+         
+        Double num;
+        Scanner sc= new Scanner(System.in);
+ 
+        System.out.print("Enter a number: ");
+        num=sc.nextDouble();
+         
+        Double square = num*num;
+        System.out.println("Square of "+ num + " is: "+ square);
+    }
+ 
+}
